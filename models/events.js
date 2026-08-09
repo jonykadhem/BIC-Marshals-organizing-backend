ab63687b633e466fec6fd6cda2152fb1b326260c
unknown <jonykadhem@gmail.com>
2026-08-09 14:33:52 +0300

    created the events model

--- models/events.js
+++ models/events.js
@@ -2,14 +2,43 @@ const mongoose = require('mongoose')
 
 const eventSchema = new mongoose.Schema({
    
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        default: '',
+    },
+    eventDate:{
+        type:Date,
+        required:true,
+    },
+    maxMarshals:{
+        type:Number,
+        min: 1,
+    },
+    registrationDeadline: {
+    type: Date,
+    required: true,
+    },
+    status: {
+    type: String,
+    enum: ["Open", "Closed", "Cancelled", "Completed"],
+    default: "Open",
+    },
+    image:{
+        type: String,
+        default: '',
+    },
+    createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    },
 
 }, {timestamps: true})
-
-eventSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        delete returnedObject.password
-    }
-})
 
 const Events = mongoose.model('Events', eventSchema)
 module.exports = Events
