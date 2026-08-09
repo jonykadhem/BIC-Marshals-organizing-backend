349815865a77ce36ecd1ae4ffe19852378f94d7e
unknown <jonykadhem@gmail.com>
2026-08-09 15:01:46 +0300

    created the create method

--- controllers/eventsPost.js
+++ controllers/eventsPost.js
@@ -5,10 +5,16 @@ const Events = require('../models/events')
 
 const create = async (req, res) => {
     try {
+        req.body.createdBy = req.user._id
+        if (req.user.role !== 'admin' || req.user.role !== 'orgnizer') {
+            return res.status(403).json({err: "Only administrators can create events."})
+        }
+        const event = await Events.create(req.body)
+        event._doc.createdBy = req.user
+        res.status(201).json(event)
 
-        
     } catch (error) {
-        console.log(error)
+        res.status(500).json({ error: error.message })
     }
 }
 
@@ -16,3 +22,27 @@ module.exports = {
 
     create,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// code grave yeared
+
+// try {
+
+//     res.status(201).json(event)
+
+// } catch (error) {
+//     res.status(500).json({ err: err.message })
+// }
