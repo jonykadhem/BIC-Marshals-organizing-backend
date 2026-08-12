a2d04aa799f6b620e17454f5cd641304032332f8
unknown <jonykadhem@gmail.com>
2026-08-12 21:04:38 +0300

    created two functions to git the users and change their roles

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,6 @@
-const User = require("../models/User")
-const Events = require("../models/Events")
-const Registration = require("../models/Registration")
+const User = require("../models/user")
+const Events = require("../models/events")
+const Registration = require("../models/registration")
 
 // Admin dashboard statistics
 const dashboard = async (req, res) => {
@@ -35,7 +35,68 @@ const dashboard = async (req, res) => {
         })
     }
 }
+// Get all users
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({})
+            .select("-password")
+            .sort({ createdAt: -1 })
+
+        res.status(200).json(users)
+
+    } catch (error) {
+        res.status(500).json({
+            err: error.message
+        })
+    }
+}
+
+// Change user role
+const updateUserRole = async (req, res) => {
+    try {
+        const { userId } = req.params
+        const { role } = req.body
+
+        // Only allow these roles to be assigned
+        if (!["marshal", "organizer"].includes(role)) {
+            return res.status(400).json({
+                err: "Invalid role."
+            })
+        }
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res.status(404).json({
+                err: "User not found."
+            })
+        }
+
+        if (user.role === "admin") {
+            return res.status(403).json({
+                err: "You can't change an admin's role."
+            })
+        }
+
+        user.role = role
+
+        await user.save()
+
+        const updatedUser = user.toObject()
+        delete updatedUser.password
+
+        res.status(200).json(updatedUser)
+
+    } catch (error) {
+        res.status(500).json({
+            err: error.message
+        })
+    }
+}
 
 module.exports = {
     dashboard,
+    getUsers,
+    updateUserRole,
+
 }
